761c0aeebfcc809e7f3768b3650b26f56820c420
Andra Popazu <andrap11@gmail.com>
2014-03-06 22:40:14 +0200

    Initialize on precomple false for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,8 @@ AppDemo::Application.configure do
   # the I18n.default_locale when a translation can not be found).
   config.i18n.fallbacks = true
 
+  config.assets.initialize_on_precompile = false
+
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
